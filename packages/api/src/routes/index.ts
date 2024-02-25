3e0662b9fad98551df1ada5502d3060f6c00e8c2
Jace Babin <jbabin91@gmail.com>
2024-02-24 18:14:48 -0700

    feat(packages): :sparkles: update types for api

--- packages/api/src/routes/index.ts
+++ packages/api/src/routes/index.ts
@@ -1,3 +1,6 @@
+import type { inferReactQueryProcedureOptions } from '@trpc/react-query';
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
+
 import { publicProcedure, router } from '../trpc';
 
 export const appRouter = router({
@@ -7,3 +10,6 @@ export const appRouter = router({
 });
 
 export type AppRouter = typeof appRouter;
+export type ReactQueryOptions = inferReactQueryProcedureOptions<AppRouter>;
+export type RouterInputs = inferRouterInputs<AppRouter>;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
