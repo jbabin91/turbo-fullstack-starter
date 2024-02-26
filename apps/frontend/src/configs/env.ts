223d609cdafe96589f0e823359e7dc05f1d797eb
Jace Babin <jbabin91@gmail.com>
2024-02-26 10:26:34 -0700

    feat(frontend): :sparkles: add env variable validation with zod

---
+++ apps/frontend/src/configs/env.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  DEV: z.boolean().default(false),
+  MODE: z.enum(['development', 'production', 'test']).default('development'),
+  PROD: z.boolean().default(false),
+  VITE_API_URL: z.string().url().default('http://localhost:8080'),
+});
+export type Environment = z.infer<typeof envSchema>;
+
+export const env = envSchema.parse(import.meta.env);
