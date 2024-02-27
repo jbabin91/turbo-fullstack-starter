4ec1ca798c0b35063161cd9a36ab891cf99e106e
Jace Babin <jbabin91@gmail.com>
2024-02-26 20:29:49 -0700

    feat(packages): :sparkles: add new getById endpoint to api package

--- packages/api/src/routes/posts/index.ts
+++ packages/api/src/routes/posts/index.ts
@@ -1,8 +1,22 @@
 import { db, posts } from '@repo/db';
+import { eq } from 'drizzle-orm';
+import { z } from 'zod';
 
 import { publicProcedure, router } from '../../trpc';
 
 export const postsRouter = router({
+  getById: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      }),
+    )
+    .query(async ({ input }) => {
+      const post = await db.query.posts.findFirst({
+        where: eq(posts.id, input.id),
+      });
+      return post;
+    }),
   list: publicProcedure.query(() => {
     const data = db.select().from(posts);
     return data;
