e455d1fd7b5f0de02c462e1915e4b0646ee6f530
Jace Babin <jbabin91@gmail.com>
2024-03-05 14:43:10 -0700

    chore(repo): :hammer: update

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   // Type check TypeScript files
   '(apps|packages|tooling)/**/*.(ts|tsx)': () => 'pnpm typecheck',
-
   // Lint then format TypeScript and JavaScript files
   '(apps|packages|tooling)/**/*.(ts|tsx|js)': (filenames) => [
     `pnpm prettier -uc ${filenames.join(' ')}`,
